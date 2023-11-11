e4dfe82263407f735ad9b3e2344c4e50323493d1
RiteshShukla77 <riteshmohan5878@gmail.com>
2023-11-11 07:24:03 +0530

    Error: DOM  JS changes not getting implemernted

---
+++ Practice Pages/DOM_Script.js
@@ -0,0 +1,6 @@
+var head = document.getElementsById('header-title');
+var mainhead = document.getElementById("main-header");
+mainhead.style.borderBottom = "solid 3px #000";
+var item = document.getElementsByClassName('title');
+item[0].style.fontWeight= 'bold';
+item[0].style.color='green'
