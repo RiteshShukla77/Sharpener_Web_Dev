9a9f889ea01dd061700b98484423a9551727f559
RiteshShukla77 <riteshmohan5878@gmail.com>
2023-11-12 19:42:16 +0530

    Sharpener Assignmet Changes: DOM Manipulation Practice Make the 3 rd element in the list have green background color Make all the elements in the list have bold color font

--- Practice Pages/DOM_Script.js
+++ Practice Pages/DOM_Script.js
@@ -1,6 +1,11 @@
-var head = document.getElementsById('header-title');
-var mainhead = document.getElementById("main-header");
-mainhead.style.borderBottom = "solid 3px #000";
-var item = document.getElementsByClassName('title');
-item[0].style.fontWeight= 'bold';
-item[0].style.color='green'
+//var head = document.getElementsById('header-title');
+var main = document.getElementById("main-header");
+main.style.borderBottom = 'solid 3px #000';
+var item = document.getElementsByClassName('list-group-item');
+item[2].style.fontWeight= 'bold';
+item[2].style.backgroundColor= 'green';
+//console.log(document.getElementsByClassName('list-group'));
+for (var i=0; i<item.length; i++){
+item[i].style.fontWeight="bold";
+item[i].style.color="red";
+}
