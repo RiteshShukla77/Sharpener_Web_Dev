92615e4e16e8ad2f687ec43993efca23e30d7f97
RiteshShukla77 <riteshmohan5878@gmail.com>
2023-11-13 02:30:51 +0530

    Sharpener Changes Make the 2nd item have green background color Make the 3rd item invisible

--- Practice Pages/DOM_Script.js
+++ Practice Pages/DOM_Script.js
@@ -1,17 +1,24 @@
-//var head = document.getElementsById('header-title');
-var main = document.getElementById("main-header");
-main.style.borderBottom = 'solid 3px #000';
-var item = document.getElementsByClassName('list-group-item');
-item[2].style.fontWeight= 'bold';
-item[2].style.backgroundColor= 'green';
-//console.log(document.getElementsByClassName('list-group'));
-for (var i=0; i<item.length; i++){
-item[i].style.fontWeight="bold";
-item[i].style.color="red";
-}
-var dummy_item = document.getElementsByClassName('Sharpener-Demo');
-dummy_item[0].style.fontWeight="bold";
-dummy_item[0].style.color="red";
+// //var head = document.getElementsById('header-title');
+// var main = document.getElementById("main-header");
+// main.style.borderBottom = 'solid 3px #000';
+// var item = document.getElementsByClassName('list-group-item');
+// item[2].style.fontWeight= 'bold';
+// item[2].style.backgroundColor= 'green';
+// //console.log(document.getElementsByClassName('list-group'));
+// for (var i=0; i<item.length; i++){
+// item[i].style.fontWeight="bold";
+// item[i].style.color="red";
+// var dummy_item = document.getElementsByClassName('Sharpener-Demo');
+// dummy_item[0].style.fontWeight="bold";
+// dummy_item[0].style.color="red";
 
-var dummy_item2 = document.getElementsByTagName('li');
-dummy_item2[4].style.color="Yellow";
+//var dummy_item2 = document.getElementsByTagName('li');
+//dummy_item2[4].style.color="Yellow";
+
+// var item = document.querySelector(".list-group-item:nth-child(2)");
+// item.style.backgroundColor= "green";
+
+var item = document.querySelectorAll(".list-group-item");
+item[1].style.backgroundColor= "green";
+item[2].style.color= "white";
+//item[2].style.backgroundColor= "green";
