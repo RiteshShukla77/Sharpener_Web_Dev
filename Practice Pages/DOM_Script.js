137195a4fad609ede17edb3252e414a6e6f3e71c
RiteshShukla77 <riteshmohan5878@gmail.com>
2023-11-14 04:29:12 +0530

    Edit button added & now you can search an element by discription as well

--- Practice Pages/DOM_Script.js
+++ Practice Pages/DOM_Script.js
@@ -23,22 +23,25 @@ function addItem(e){
   li.className = 'list-group-item';
   // Add text node with input value
   li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(" "+discription));
 
   // Create del button element
   var deleteBtn = document.createElement('button');
-  var text = document.createElement('text');
+  var edit = document.createElement('button');
 
   // Add classes to del button
   deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-  text.textContent=" "+discription;
+  edit.className='btn btn-danger btn-sm float-right delete';
+  edit.style="background-color: blanchedalmond; color: #000;"
   
 
   // Append text node
   deleteBtn.appendChild(document.createTextNode('X'));
+  edit.appendChild(document.createTextNode('Edit'));
 
   // Append button to li
   li.appendChild(deleteBtn);
-  li.appendChild(text);
+  li.appendChild(edit);
 
   // Append li to list
   itemList.appendChild(li);
@@ -54,18 +57,22 @@ function removeItem(e){
   }
 }
 
-// Filter Items
 function filterItems(e){
   // convert text to lowercase
   var text = e.target.value.toLowerCase();
-  // Get lis
+
   var items = itemList.getElementsByTagName('li');
   // Convert to an array
   Array.from(items).forEach(function(item){
     var itemName = item.firstChild.textContent;
-    if(itemName.toLowerCase().indexOf(text) != -1){
+    var description = item.childNodes[1].textContent;
+    if(itemName.toLowerCase().indexOf(text) !=-1){
       item.style.display = 'block';
-    } else {
+    }
+    else if( description.toLowerCase().indexOf(text)  != -1){
+        item.style.display="block";
+    }
+    else {
       item.style.display = 'none';
     }
   });
